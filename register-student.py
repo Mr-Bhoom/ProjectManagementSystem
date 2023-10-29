20cd409c138b278d9a2507d9f36a079ffeb31f3a
KomalRamraoJadhav <127689127+KomalRamraoJadhav@users.noreply.github.com>
2023-10-29 23:39:10 +0530

    Update register-student.py

--- register-student.py
+++ register-student.py
@@ -1,46 +1,57 @@
-from flask import Flask, request, render_template,session
+from flask import Flask, request, render_template,session,redirect,url_for
 import mysql.connector
 
 app=Flask(__name__)
 
 
-db = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='123',
-    database='projectmanagementsystem'
-)
-@app.route('/')
+db = {
+    'host':'localhost',
+    'user':'root',
+    'password':'Anuja@2002',
+    'database':'projectmanagementsystem'
+}
 
-@app.route('/Register-student.html',methods=['GET','POST'])
+@app.route('/')
 def registration_form():
-    print(request.args)
-    return render_template('Register-student.html')
+    return render_template('register_student.html')
+
+# @app.route('/Register-student.html',methods=['GET','POST'])
+# def registration_form():
+#     print(request.args)
+#     return render_template('register_student.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    cursor = db.cursor()
-    full_name = request.form.get['fullname']
-    S_email = request.form.get['email']
-    S_Department= request.form.get['department']
-    Year = request.form.get['year']
-    Roll_no=request.form.get['RollNo']
-    batch=request.form.get['Batch']
-    prn = request.form.get['username']
-    S_password=request.form.get['password']
-    
-  
-    
-    
+    if request.method=='POST':
+        
+        full_name = request.form['fullname']
+        S_email = request.form['email']
+        S_Department= request.form['department']
+        Year = request.form['year']
+        Roll_no=request.form['RollNo']
+        batch=request.form['Batch']
+        prn = request.form['username']
+        S_password=request.form['password']
 
-    insert_query = "INSERT INTO student (full_name,prn,S_Department,S_email,S_password,Year,Roll_no,batch) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(insert_query, (full_name,prn,S_Department,S_email,S_password,Year,Roll_no,batch))
-    db.commit()
-
+        try:
     
+            con=mysql.connector.connect(**db)
+            cursor = con.cursor()
+        
+        
 
-    cursor.close()
-    return "Registration successful."
+            insert_query = "INSERT INTO student (full_name,prn,S_Department,S_email,S_password,Year,Roll_no,batch) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+            data=(full_name,prn,S_Department,S_email,S_password,Year,Roll_no,batch)
+            cursor.execute(insert_query,data )
+            con.commit()
+            cursor.close()
+            con.close()
+
+            return "Registration successful."
+        except Exception as e:
+            return str(e)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
